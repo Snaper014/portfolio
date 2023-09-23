1e73b8bbb4f6eba1702eed51d8756ff9bd8009fd
Nelson <nelson.vico@gmail.com>
2023-09-23 17:52:44 +0200

    modification echappement

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className='h-[100vh] text-2xl w-full bg-Home text-[white] flex flex-col items-center gap-2'>
-      <h2>Not Found</h2>
-      <p >Could not find requested resource</p>
+      <h2>Error 404</h2>
+      <p>Chemin non trouver</p>
       <Link href="/">Retour à l'accueil</Link>
     </div>
   )
